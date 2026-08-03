6a72ca47f0847191504b30eddbc6f8fb5bf261c7
ksolo21-web <ksolo21@gmail.com>
2026-08-03 13:47:19 -0400

    Bump KREATIV canvas fix build to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.kreativstudio.app"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
