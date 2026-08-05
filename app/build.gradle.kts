de6f57552b5d88cb79783191b402fd50931c9c23
ksolo21-web <ksolo21@gmail.com>
2026-08-04 21:37:52 -0400

    Bump KREATIV cloud recovery build to 0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.kreativstudio.app"
         minSdk = 28
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
